86d513ba58e4fd34bc17ac780375fc8b521b7863
Nicos027 <160978543+Nicos027@users.noreply.github.com>
2026-04-05 15:49:57 -0400

    Create ads131m02.h
    ADS driver skeleton

---
+++ ads131m02.h
@@ -0,0 +1,44 @@
+//
+//  ads131m02.h
+//  Power Quality Analyzer
+//
+//  Created by Nicos Eftychiou on 4/5/26.
+//
+
+#ifndef ads131m02_h
+#define ads131m02_h
+
+
+#endif /* ads131m02_h */
+
+#pragma once
+
+#include <csdint
+#include <string>
+
+namespace std
+
+struct SampleFrame {
+    int32_t ch0_raw;
+    int32_t ch1_raw;
+};
+
+class ADS131M02 {
+public:
+    ADS131M02(const string& spiDevice, iunt32_t speedHz);
+    ~ADS131M02();
+    
+    bool openDevice();
+    void closeDevice();
+    
+    bool configure();
+    bool readSample(SampleFrame& frame);
+    
+private:
+    string spiDevice_;
+    uint32_t speedHz_;
+    int fd_;
+    
+    bool transfer(const uint8_t* tx, uint8_t* rx, size_t len);
+    int32_t signed24(unit8_t b0, uint8_t b1, uint8_t b2);
+};
