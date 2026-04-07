265e6f5182ea54df31904a9892d74ccb2a1dcdac
Nicos027 <160978543+Nicos027@users.noreply.github.com>
2026-04-06 20:36:01 -0400

    Create signal_processing.c

---
+++ signal_processing.c
@@ -0,0 +1,45 @@
+//
+//  signal_processing.cpp
+//  Power Quality Analyzer
+//
+//  Created by Nicos Eftychiou on 4/6/26.
+//  Sample storage
+
+#include "signal_processing.hpp"
+
+using namespace std;
+
+// stores recent samples
+RollingBuffer::RollingBuffer(size_t maxSize) : maxSize {}
+
+void RollingBuffer::push(double value) {
+    if (data_.size() >= maxSize_) {
+        data_.pop_front();
+    }
+    data_.push_back(value);
+}
+
+vector<double> RollingBuffer::latest(size_t count) const {
+    if (count > data_.size()) {
+        count = data_.size();
+    }
+    return vector<double>(data_.end() - count, data_.end());
+}
+
+size RollingBuffer::size() const {
+    return data_.size();
+}
+
+// calculates RMS from block of samples
+// square each sample
+// average them
+// take sqrt
+double computeRMS(const vector<double>& x) {
+    if (x.empty()) return 0.0;
+    
+    double sumSq = 0.0;
+    for (double v : x) {
+        sumsSq += v * v;
+    }
+    return sqrt(sumSq / static_cast<double>(x.size()));
+}
