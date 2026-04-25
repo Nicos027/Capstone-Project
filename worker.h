5cdbaf2e113b26a459dad3730c58f88171727648
Nicos027 <160978543+Nicos027@users.noreply.github.com>
2026-04-25 19:59:21 -0400

    Create worker.h

---
+++ worker.h
@@ -0,0 +1,34 @@
+#ifndef WORKER_H
+#define WORKER_H
+
+#include <QObject>
+#include <QThread>
+#include <QVector>
+#include <QString>
+#include <atomic>
+
+class Worker : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Worker(QObject *parent = nullptr);
+    ~Worker();
+
+    void stop();
+
+public slots:
+    void run();
+
+signals:
+    void newReadings(double vrms, double irms, const QString& alarm);
+    void newWaveform(const QVector<double>& voltage,
+                     const QVector<double>& current);
+    void alarmTriggered(const QString& alarmType, double vrms, double irms);
+    void finished();
+    void errorMessage(const QString& msg);
+
+private:
+    std::atomic<bool> running_;
+};
+
+#endif
